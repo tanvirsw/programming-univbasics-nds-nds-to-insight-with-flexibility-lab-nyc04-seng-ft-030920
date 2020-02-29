5df68993c3f0783898d3454fc736a0a57205c903
Tanvir Walia <tarywalia@gmail.com>
2020-02-29 23:42:05 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -53,7 +53,7 @@ def movies_with_director_key(name, movies_collection)
   i = 0
   while i < movies_collection.length do
     movie_data = movies_collection[i]
-    result << movie_with_director_name[name,movie_data]
+    result << movie_with_director_name(name,movie_data)
     i += 1
   end
   result
