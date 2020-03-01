8e473dac5ed1684184ce25b0dfd0de553eacdf96
Tanvir Walia <tarywalia@gmail.com>
2020-03-01 00:01:00 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -75,7 +75,7 @@ def gross_per_studio(collection)
   result = {}
   i = 0
 
-  while i < collection.lengh do
+  while i < collection.length do
     movie = collection[i]
 
     if !result[movie[:studio]]
